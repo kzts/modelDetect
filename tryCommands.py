ed090be480882f42bfd6df8dd89e452d40e9b28b
kzts <tanaka@isi.imi.i.u-tokyo.ac.jp>
2017-07-28 11:35:01 +0900

    mode

--- tryCommands.py
+++ tryCommands.py
@@ -1,43 +1,48 @@
 # Coding: utf-8
 import os
 import time
+import sys
 
+if len( sys.argv ) != 2:
+    print 'input mode'
+    quit()
+mode  = sys.argv[1];
+
+# number: back0 = 0, forw0 = 1, forw1 = 2, back1 = 3
 swing = './swing'
+pmin = 0.1
+pfix = 0.2
 
-# number
-#forw0 = 0
-#back0 = 1
-#forw1 = 2
-#back1 = 3
-
-fix = 0.3
-#min0 = 0.1
-#min1 = 0.1
-#diff = 0.1
-
-for min0 in [ 0.05, 0.10, 0.15, 0.20, 0.25 ]:
-    for min1 in [ 0.05, 0.10, 0.15, 0.20, 0.25 ]:
-        for diff in [ 0.06, 0.07, 0.08, 0.09, 0.10 ]:
-#        for diff in [ 0.05, 0.06, 0.07, 0.07, 0.08 ]:
-#for min0 in [ 0.05, 0.25 ]:
-#    for min1 in [ 0.05, 0.25 ]:
-#        for diff in [ 0.05, 0.08 ]:
-            # set pressure
-            back0 = min0
-            back1 = min1 + fix
-            forw0 = min0 + diff
-            forw1 = min1
-            #back0 = min0 + fix
-            #back1 = min1
-            #forw0 = min0
-            #forw1 = min1 + diff
-            # set buffuer
-            command = swing + ' ' + str( forw0 ) + ' '+ str( back0 ) + ' '+ str( forw1 ) + ' '+ str( back1 )
+if mode == '0':
+    for diff0 in [ 0.11, 0.12, 0.13, 0.14, 0.15 ]:
+        for diff1 in [ 0.11, 0.12, 0.13, 0.14, 0.15 ]:
+            back0 = str( pmin )
+            forw0 = str( pmin + diff0 )            
+            back1 = str( pfix + diff1 )
+            forw1 = str( pfix )
+            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
             print command
-            # execute
             os.system(command)
-            # wait
             time.sleep(1)
-
-#for n in range(0,repeat_n):
-#   os.system(command)
+elif mode == '1':
+    for diff0 in [ 0.11, 0.12, 0.13, 0.14, 0.15 ]:
+        for diff1 in [ 0.11, 0.12, 0.13, 0.14, 0.15 ]:
+            back0 = str( pfix + diff0 )
+            forw0 = str( pfix )
+            back1 = str( pmin )
+            forw1 = str( pmin + diff1 )            
+            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
+            print command
+            os.system(command)
+            time.sleep(1)
+else:
+    for diff0 in [ 0.06, 0.07, 0.08, 0.09, 0.10 ]:
+        for diff1 in [ 0.06, 0.07, 0.08, 0.09, 0.10 ]:
+            back0 = str( pmin )
+            forw0 = str( pmin + diff0 )
+            back1 = str( pmin )
+            forw1 = str( pmin + diff1 )            
+            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
+            print command
+            os.system(command)
+            time.sleep(1)
