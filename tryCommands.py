0f886c8441536fc5840f9e37976a4efff1ad0dcb
kzts <tanaka@isi.imi.i.u-tokyo.ac.jp>
2017-07-29 14:05:42 +0900

    twice pressure difference

--- tryCommands.py
+++ tryCommands.py
@@ -10,40 +10,27 @@ mode  = sys.argv[1];
 
 # number: back0 = 0, forw0 = 1, forw1 = 2, back1 = 3
 swing = './swing'
-pmin = 0.15
-pfix = 0.15
+pmin = 0.20
+pfix = 0.20
 
-if mode == '0':
-    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
-        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+for diff0 in [ 0.04, 0.08, 0.12, 0.16, 0.20 ]:
+    for diff1 in [ 0.04, 0.08, 0.12, 0.16, 0.20 ]:
+        if mode == '0':
             back0 = str( pmin - diff0 )
             forw0 = str( pmin + diff1 )            
             back1 = str( pmin )
             forw1 = str( pmin + pfix )
-            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
-            print command
-            os.system(command)
-            time.sleep(1)
-elif mode == '1':
-    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
-        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+        elif mode == '1':
             back0 = str( pmin )
             forw0 = str( pmin + pfix )
             back1 = str( pmin - diff0 )
             forw1 = str( pmin + diff1 )
-            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
-            print command
-            os.system(command)
-            time.sleep(1)        
-else:
-    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
-        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+        else:
             back0 = str( pmin )
             forw0 = str( pmin + diff0 )
             back1 = str( pmin )
             forw1 = str( pmin + diff1 )
-            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
-            print command
-            os.system(command)
-            time.sleep(1)                    
-
+        command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
+        print command
+        os.system(command)
+        time.sleep(1)
