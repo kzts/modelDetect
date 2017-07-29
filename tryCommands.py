ebe3b642e3c9e5247a86c72b8910e6ae677c8694
kzts <tanaka@isi.imi.i.u-tokyo.ac.jp>
2017-07-29 12:47:11 +0900

    +-

--- tryCommands.py
+++ tryCommands.py
@@ -10,32 +10,34 @@ mode  = sys.argv[1];
 
 # number: back0 = 0, forw0 = 1, forw1 = 2, back1 = 3
 swing = './swing'
-pmin = 0.1
-pfix = 0.2
+pmin = 0.15
+pfix = 0.15
 
 if mode == '0':
-    for diff in [ 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40 ]:
-        back0 = str( pmin )
-        forw0 = str( pmin + diff )            
-        back1 = str( pmin  )
-        forw1 = str( pmin + pfix )
-        command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
-        print command
-        os.system(command)
-        time.sleep(1)
+    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+            back0 = str( pmin - diff0 )
+            forw0 = str( pmin + diff1 )            
+            back1 = str( pmin )
+            forw1 = str( pmin + pfix )
+            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
+            print command
+            os.system(command)
+            time.sleep(1)
 elif mode == '1':
-    for diff in [ 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40 ]:
-        back0 = str( pmin )
-        forw0 = str( pmin + pfix )
-        back1 = str( pmin )
-        forw1 = str( pmin + diff )
-        command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
-        print command
-        os.system(command)
-        time.sleep(1)        
+    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+            back0 = str( pmin )
+            forw0 = str( pmin + pfix )
+            back1 = str( pmin - diff0 )
+            forw1 = str( pmin + diff1 )
+            command = swing + ' ' + mode + ' ' + back0 + ' ' + forw0 + ' ' + forw1 + ' '+ back1
+            print command
+            os.system(command)
+            time.sleep(1)        
 else:
-    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16 ]:
-        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16 ]:
+    for diff0 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
+        for diff1 in [ 0.02, 0.04, 0.06, 0.08, 0.10 ]:
             back0 = str( pmin )
             forw0 = str( pmin + diff0 )
             back1 = str( pmin )
